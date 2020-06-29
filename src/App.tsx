289783614ddf832fe94416e326014e8cccaad0f6
McPqndq <kellybob3737@gmail.com>
2020-06-29 15:46:30 -0500

    completed parser

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import pako from 'pako';
+import NBTController from './NBTController';
+
+const InitializeNBT = (f: File): Promise<NBTController | undefined> => {
+  return new Promise(resolve => {
+    const reader = new FileReader();
+    reader.onload = e => {
+      if(!e.target?.result || typeof e.target?.result !== 'object') throw new Error(`Error getting binary data for ${f.name}`);
+      let data = new Uint8Array(e.target.result);
+      if(data[0] === 0x1f && data[1] === 0x8b) data = pako.inflate(data);
+      resolve(new NBTController(f.name, data));
+    }
+    reader.readAsArrayBuffer(f);
+  });
+}
+
+type DisplayData = 
+  {type: 'nbt', nbt: NBTController} |
+  {type: 'error', error: string} |
+  {type: 'waiting'}
 
 function App() {
+  const [data, setData] = useState<DisplayData>({type:'waiting'});
+  const uploadHandler = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if(!e.target.files) return;
+    try{
+      const nbt = await InitializeNBT(e.target.files[0]);
+      if(nbt) setData({type:'nbt',nbt});
+      else setData({type:'waiting'});
+    }catch(e){
+      setData({type:'error', error:String(e)})
+    }
+  }
   return (
     <div>
-      Hello World
+      <input type="file" onChange={uploadHandler}/>
+      {data.type==='nbt'?(
+        <div>
+          {data.nbt.name}
+        </div>
+      ):data.type==='error'?(
+        <div>
+          {data.error}
+        </div>
+      ):''}
     </div>
   );
 }
---
+++ src/NBTController.ts
@@ -0,0 +1,114 @@
+export enum TagTypes{ end, byte, short, int, long, float, double, byteArray, string, list, compound, intArray, longArray };
+type Pair<Tag, Type> = {type: Tag, value: Type};
+type List<Tag extends TagTypes> = Pair<TagTypes.list, TagTypeTypes[Tag][]> | {listType: Tag};
+export type TagTypeTypes = {
+  [TagTypes.end]: Pair<TagTypes.end, 0>;
+  [TagTypes.byte]: Pair<TagTypes.byte, number>;
+  [TagTypes.short]: Pair<TagTypes.short, number>;
+  [TagTypes.int]: Pair<TagTypes.int, number>;
+  [TagTypes.long]: Pair<TagTypes.long, bigint>;
+  [TagTypes.float]: Pair<TagTypes.float, number>;
+  [TagTypes.double]: Pair<TagTypes.double, number>;
+  [TagTypes.byteArray]: Pair<TagTypes.byteArray, number[]>;
+  [TagTypes.string]: Pair<TagTypes.string, string>;
+  [TagTypes.list]: List<TagTypes>;
+  [TagTypes.compound]: Pair<TagTypes.compound, {[key in string]?: TagTypeTypes[TagTypes]}>;
+  [TagTypes.intArray]: Pair<TagTypes.intArray, number[]>;
+  [TagTypes.longArray]: Pair<TagTypes.longArray, bigint[]>;
+}
+
+export default class NBTController{
+  tag: TagTypeTypes[TagTypes.compound];
+  constructor(public name: string, data: Uint8Array){
+    const reader = new NBTReader(data);
+    const compoundTag = reader[TagTypes.byte]();
+    if(compoundTag.value !== TagTypes.compound) throw new Error('Root Tag should be compound');
+    this.tag = {
+      type: TagTypes.compound,
+      value: {
+        [reader[TagTypes.string]().value]: reader[TagTypes.compound](),
+      },
+    }
+  }
+}
+
+class NBTReader{
+  data: DataView;
+  offset: number = 0;
+
+  constructor(data: Uint8Array){
+    this.data = new DataView(data.buffer);
+  }
+
+  [TagTypes.end](){ return { value: 0, type: TagTypes.end } as TagTypeTypes[TagTypes.end]; }
+  [TagTypes.byte](){
+    const value = this.data.getInt8(this.offset);
+    this.offset +=1 ;
+    return { value, type: TagTypes.byte } as TagTypeTypes[TagTypes.byte];
+  }
+  [TagTypes.short](){
+    const value = this.data.getInt16(this.offset);
+    this.offset += 2;
+    return { value, type: TagTypes.short } as TagTypeTypes[TagTypes.short];
+  }
+  [TagTypes.int]() {
+    const value = this.data.getInt32(this.offset);
+    this.offset += 4;
+    return { value, type: TagTypes.int } as TagTypeTypes[TagTypes.int];
+  }
+  [TagTypes.long](){
+    const value = this.data.getBigInt64(this.offset);
+    this.offset += 4;
+    return { value, type: TagTypes.long } as TagTypeTypes[TagTypes.long];
+  }
+  [TagTypes.float](){
+    const value = this.data.getFloat32(this.offset);
+    this.offset += 4;
+    return { value, type: TagTypes.float } as TagTypeTypes[TagTypes.float];
+  }
+  [TagTypes.double](){
+    const value = this.data.getFloat64(this.offset);
+    this.offset += 8;
+    return { value, type: TagTypes.double } as TagTypeTypes[TagTypes.double];
+  }
+  [TagTypes.byteArray](){
+    const len = this[TagTypes.int]().value;
+    const value: number[] = [];
+    for(let i = 0; i < len; i++) value.push(this[TagTypes.byte]().value);
+    return { value, type: TagTypes.byteArray } as TagTypeTypes[TagTypes.byteArray];
+  }
+  [TagTypes.string](){
+    const len = this[TagTypes.short]().value;
+    const slice = this.data.buffer.slice(this.offset, this.offset + len);
+    this.offset += len;
+    return { value: (new TextDecoder('utf-8')).decode(slice), type: TagTypes.string } as TagTypeTypes[TagTypes.string];
+  }
+  [TagTypes.list](){
+    const type: TagTypes = this[TagTypes.byte]().value;
+    const len = this[TagTypes.int]().value;
+    const value: TagTypeTypes[TagTypes][] = [];
+    for(let i = 0; i < len; i++) value.push(this[type]()); 
+    return { value, type: TagTypes.list } as TagTypeTypes[TagTypes.list];
+  }
+  [TagTypes.compound](){
+    const tag: {[key: string]: TagTypeTypes[TagTypes]} = {};
+    while(true){
+      const type: TagTypes = this[TagTypes.byte]().value;
+      if(type === TagTypes.end) break;
+      tag[this[TagTypes.string]().value] = this[type]();
+    }
+    return { value: tag, type: TagTypes.compound } as TagTypeTypes[TagTypes.compound];
+  }
+  [TagTypes.intArray](){
+    const len = this[TagTypes.int]().value;
+    const value: number[] = [];
+    for(let i = 0; i < len; i++) value.push(this[TagTypes.int]().value);
+    return { value, type: TagTypes.intArray } as TagTypeTypes[TagTypes.intArray];
+  }
+  [TagTypes.longArray](){
+    const len = this[TagTypes.int]().value;
+    const value: bigint[] = [];
+    for(let i = 0; i < len; i++) value.push(this[TagTypes.long]().value);
+    return { value, type: TagTypes.longArray } as TagTypeTypes[TagTypes.longArray];
+  }
+}
